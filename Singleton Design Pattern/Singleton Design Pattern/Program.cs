6c753268c690dcc3738e6a112e669118c8b38b34
mosic13 <monali.chauhan@gmail.com>
2016-03-20 01:00:12 +0530

    Initial commit
    Initial commit

---
+++ Singleton Design Pattern/Singleton Design Pattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SingletonProject;
+
+namespace Singleton_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton a = Singleton.GetInstance();
+            Console.WriteLine("Singleton: {0}, Hash:{1}", a.Name == null ? "null  " : a.Name, a.GetHashCode());
+
+            a.Name = "Monali";
+
+            Singleton b = Singleton.GetInstance();
+            Console.WriteLine("Singleton: {0}, Hash:{1}", b.Name, b.GetHashCode());
+
+            Singleton c = Singleton.GetInstance();
+            Console.WriteLine("Singleton: {0}, Hash:{1}", c.Name, c.GetHashCode());
+
+            Singleton d = Singleton.GetInstance();
+            Console.WriteLine("Singleton: {0}, Hash:{1}", d.Name, d.GetHashCode());
+
+            a.Count = 15;
+            Singleton e = Singleton.GetInstance();
+            Console.WriteLine("Singleton: {0}, Hash:{1}", e.Name, e.GetHashCode());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton Design Pattern/SingletonProject/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingletonProject
+{
+    public class Singleton
+    {
+        //static member
+        private static Singleton singleInstance = null;
+
+        public int Count { get; set; }
+        public string Name { get; set; }
+
+
+        //hides the default constructor
+        private Singleton()
+        {
+            Console.WriteLine("Singleton: Initializing Instance");
+        }
+
+        public static Singleton GetInstance()
+        {
+            //Checks if the object is not created
+            if (singleInstance == null)
+                singleInstance = new Singleton();
+
+            return singleInstance;
+        }
+    }
+}
