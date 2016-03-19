974041a364c2e84ce5ef272b254403a76177481d
mosic13 <monali.chauhan@gmail.com>
2016-03-20 01:11:01 +0530

    comments added appropriately
    comments added appropriately

--- Singleton Design Pattern/SingletonProject/Singleton.cs
+++ Singleton Design Pattern/SingletonProject/Singleton.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,22 +7,23 @@ namespace SingletonProject
 {
     public class Singleton
     {
-        //static member
+        //Step 2 : static member
         private static Singleton singleInstance = null;
 
         public int Count { get; set; }
         public string Name { get; set; }
 
 
-        //hides the default constructor
+        //Step 1 : hides the default constructor
         private Singleton()
         {
             Console.WriteLine("Singleton: Initializing Instance");
         }
 
+        //Step 3 : Make a static public method
         public static Singleton GetInstance()
         {
-            //Checks if the object is not created
+            //Step 3 : Checks if the object is not created
             if (singleInstance == null)
                 singleInstance = new Singleton();
 
